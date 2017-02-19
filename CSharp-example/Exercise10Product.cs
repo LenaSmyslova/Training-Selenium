a4e11affefa98cbb470f4e13f9e1038f2f25682a
Smyslova Lena <safronova_helen@mail.ru>
2017-02-19 19:21:39 +0200

    Fix Ex10

--- CSharp-example/Exercise10Product.cs
+++ CSharp-example/Exercise10Product.cs
@@ -31,9 +31,34 @@ namespace CSharp_example
             driver.Url = "http://localhost/litecart/en/";
             wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("img[title='My Store']")));
 
-            //Get Product data from main page
-            IWebElement product = driver.FindElement(By.CssSelector("div#box-campaigns li.product:nth-of-type(1)"));
+            //Get and check Product data from main page
+            IWebElement product = driver.FindElement(By.CssSelector("div#box-most-popular li.product:first-child"));
+            var productName = product.FindElement(By.CssSelector("div.name")).GetAttribute("textContent");
+            string productPriceRegular = string.Empty, productPriceSale = string.Empty, productPrice = string.Empty;
+            if (product.FindElements(By.CssSelector("div.sticker.sale")).Count != 0)
+            {
+                productPriceRegular = product.FindElement(By.CssSelector("s.regular-price")).GetAttribute("textContent");
+                productPriceSale = product.FindElement(By.CssSelector("strong.campaign-price")).GetAttribute("textContent");
+            }
+            else
+            {
+                productPrice = product.FindElement(By.CssSelector("span.price")).GetAttribute("textContent");
+            }
+            product.Click();
+            wait.Until(ExpectedConditions.TitleContains("| Rubber Ducks | My Store"));
 
+            //Get and check Product data from Product Details page
+            IWebElement productDetails = driver.FindElement(By.CssSelector("div#box-product.box"));
+            Assert.AreEqual(productName, productDetails.FindElement(By.CssSelector("h1")).GetAttribute("textContent"));
+            if (productDetails.FindElements(By.CssSelector("div.sticker.sale")).Count != 0)
+            {
+                Assert.AreEqual(productPriceRegular, productDetails.FindElement(By.CssSelector("s.regular-price")).GetAttribute("textContent"));
+                Assert.AreEqual(productPriceSale, productDetails.FindElement(By.CssSelector("strong.campaign-price")).GetAttribute("textContent"));
+            }
+            else
+            {
+                Assert.AreEqual(productPrice, productDetails.FindElement(By.CssSelector("span.price")).GetAttribute("textContent"));
+            }
         }
 
         [TearDown]
