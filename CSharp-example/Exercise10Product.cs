a08d5d98aba067011274848e99486a0d74011e23
Smyslova Lena <safronova_helen@mail.ru>
2017-02-23 01:36:16 +0200

    Ex10 fix

--- CSharp-example/Exercise10Product.cs
+++ CSharp-example/Exercise10Product.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.IE;
+//using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
-using System.Collections.Generic;
+using OpenQA.Selenium.Remote;
 
 namespace CSharp_example
 {
@@ -19,7 +20,7 @@ namespace CSharp_example
         [SetUp]
         public void start()
         {
-            driver = new InternetExplorerDriver();
+            driver = new ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
 
@@ -33,39 +34,60 @@ namespace CSharp_example
 
             //Get and check Product data from main page
             IWebElement product = driver.FindElement(By.CssSelector("div#box-most-popular li.product:first-child"));
-            var productName = product.FindElement(By.CssSelector("div.name")).GetAttribute("textContent");
-            string productPriceRegular = string.Empty, productPriceSale = string.Empty, productPrice = string.Empty;
-            if (product.FindElements(By.CssSelector("div.sticker.sale")).Count != 0)
-            {
-                productPriceRegular = product.FindElement(By.CssSelector("s.regular-price")).GetAttribute("textContent");
-                productPriceSale = product.FindElement(By.CssSelector("strong.campaign-price")).GetAttribute("textContent");
-            }
-            else
-            {
-                productPrice = product.FindElement(By.CssSelector("span.price")).GetAttribute("textContent");
-            }
-            product.Click();
-            wait.Until(ExpectedConditions.TitleContains("| Rubber Ducks | My Store"));
+            priceStyleCheck(product, "test");
+            //var productName = product.FindElement(By.CssSelector("div.name")).GetAttribute("textContent");
+            //string productPriceRegular = string.Empty, productPriceSale = string.Empty, productPrice = string.Empty;
+            //if (product.FindElements(By.CssSelector("div.sticker.sale")).Count != 0)
+            //{
+            //    productPriceRegular = product.FindElement(By.CssSelector("s.regular-price")).GetAttribute("textContent");
+            //    productPriceSale = product.FindElement(By.CssSelector("strong.campaign-price")).GetAttribute("textContent");
+            //}
+            //else
+            //{
+            //    productPrice = product.FindElement(By.CssSelector("span.price")).GetAttribute("textContent");
+            //}
+            //product.Click();
+            //wait.Until(ExpectedConditions.TitleContains("| Rubber Ducks | My Store"));
 
-            //Get and check Product data from Product Details page
-            IWebElement productDetails = driver.FindElement(By.CssSelector("div#box-product.box"));
-            Assert.AreEqual(productName, productDetails.FindElement(By.CssSelector("h1")).GetAttribute("textContent"));
-            if (productDetails.FindElements(By.CssSelector("div.sticker.sale")).Count != 0)
-            {
-                Assert.AreEqual(productPriceRegular, productDetails.FindElement(By.CssSelector("s.regular-price")).GetAttribute("textContent"));
-                Assert.AreEqual(productPriceSale, productDetails.FindElement(By.CssSelector("strong.campaign-price")).GetAttribute("textContent"));
-            }
-            else
-            {
-                Assert.AreEqual(productPrice, productDetails.FindElement(By.CssSelector("span.price")).GetAttribute("textContent"));
-            }
+            ////Get and check Product data from Product Details page
+            //IWebElement productDetails = driver.FindElement(By.CssSelector("div#box-product.box"));
+            //Assert.AreEqual(productName, productDetails.FindElement(By.CssSelector("h1")).GetAttribute("textContent"));
+            //if (productDetails.FindElements(By.CssSelector("div.sticker.sale")).Count != 0)
+            //{
+            //    Assert.AreEqual(productPriceRegular, productDetails.FindElement(By.CssSelector("s.regular-price")).GetAttribute("textContent"));
+            //    Assert.AreEqual(productPriceSale, productDetails.FindElement(By.CssSelector("strong.campaign-price")).GetAttribute("textContent"));
+            //}
+            //else
+            //{
+            //    Assert.AreEqual(productPrice, productDetails.FindElement(By.CssSelector("span.price")).GetAttribute("textContent"));
+            //}
+        }
+
+        public void priceStyleCheck(IWebElement product, string selector)
+        {
+            ICapabilities capabilities = ((RemoteWebDriver)driver).Capabilities;
+            var browser = capabilities.BrowserName;
+            //switch (browser)
+            //{
+            //    case "internet explorer":
+
+            //        break;
+
+            //    case "firefox":
+
+            //        break;
+
+            //    case "chrome":
+
+            //        break;
+            //}
         }
 
         [TearDown]
         public void stop()
         {
             driver.Quit();
-            driver = null;
+            driver = null; 
         }
     }
 }
