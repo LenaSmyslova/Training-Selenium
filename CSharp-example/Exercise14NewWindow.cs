1c05661410b27de8b00984d21908ac39a080c689
Smyslova Lena <safronova_helen@mail.ru>
2017-02-28 02:03:29 +0200

    Add Ex14

---
+++ CSharp-example/Exercise14NewWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace CSharp_example
+{
+    /// <summary>
+    /// Exercise14: "New Window" scenario for Selenium3 course
+    /// </summary>
+    [TestFixture]
+    public class Exercise14NewWindow
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new FirefoxDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Exercise14NewWindowTest()
+        {
+            //Login
+            driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("img[title='My Store']")));
+
+            //Navigate to Add Country page
+            driver.FindElement(By.CssSelector("a.button")).Click();
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
